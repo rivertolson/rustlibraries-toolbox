c095c2ac5dbee3a39d05f176b179f6aec83fb51d
rivertolsonWSU <rivertolson@mail.weber.edu>
2025-04-30 15:11:11 -0600

    Added string module

--- src/lib.rs
+++ src/lib.rs
@@ -2,85 +2,98 @@
 //! 
 //! This crate is a group of useful code snippets
 //! 
-//! ## Functions
-//! - random_special()
+//! ## Modules
+//! - strings
 //! 
+//! ## External Libraries
+//! - `fastrand`
+
+//! # strings
+//!
+//! A collection of functions that operate on strings.
+//!
+//! ## Features
+//! - random_special
+//!
 //! ## Examples
 //! ### random_special
 //! Generates a random special character from the following:
 //! ~`!@#$%^&*()_+-=[{]}\|;:'",<.>/?
 //! 
 //! ``` rust
-//! use toolbox::{random_special};
+//! use toolbox::strings::{random_special};
 //! 
 //! fn main() {
 //!     let my_char: char = random_special();
 //!     println!("'{}'", my_char);
 //! }
-//! ```
-//! 
+//! ````
+//!
 //! ## Modules
 //! - `fastrand`
+pub mod strings{
+    use fastrand;
 
-use fastrand;
+    /// A character array of special characters
+    pub const SPECIAL_CHARACTERS: [char;32] = [
+        '~',
+        '`',
+        '!',
+        '@',
+        '#',
+        '$',
+        '%',
+        '^',
+        '&',
+        '*',
+        '(',
+        ')',
+        '-',
+        '_',
+        '=',
+        '+',
+        '[',
+        '{',
+        ']',
+        '}',
+        '\\',
+        '|',
+        ';',
+        ':',
+        '\"',
+        '\'',
+        ',',
+        '<',
+        '.',
+        '>',
+        '/',
+        '?',
+    ];
 
-const SPECIAL_CHARACTERS: [char;32] = [
-    '~',
-    '`',
-    '!',
-    '@',
-    '#',
-    '$',
-    '%',
-    '^',
-    '&',
-    '*',
-    '(',
-    ')',
-    '-',
-    '_',
-    '=',
-    '+',
-    '[',
-    '{',
-    ']',
-    '}',
-    '\\',
-    '|',
-    ';',
-    ':',
-    '\"',
-    '\'',
-    ',',
-    '<',
-    '.',
-    '>',
-    '/',
-    '?',
-];
-
-/// Generates a random special character from the following:
-/// ~`!@#$%^&*()_+-=[{]}\|;:'",<.>/?
-/// 
-/// # Arguments
-/// None
-/// 
-/// # Returns
-/// char
-/// 
-/// # Examples
-/// ``` rust
-/// use toolbox::{random_special};
-/// 
-/// fn main() {
-///     let my_char: char = random_special();
-///     println!("'{}'", my_char);
-/// }
-/// ```
-pub fn random_special() -> char {
-    let ran_num: u8 = fastrand::u8(0..32);
-    SPECIAL_CHARACTERS[ran_num as usize]
+    /// Generates a random special character from the following:
+    /// ~`!@#$%^&*()_+-=[{]}\|;:'",<.>/?
+    /// 
+    /// # Arguments
+    /// None
+    /// 
+    /// # Returns
+    /// char
+    /// 
+    /// # Examples
+    /// ``` rust
+    /// use toolbox::strings::{random_special};
+    /// 
+    /// fn main() {
+    ///     let my_char: char = random_special();
+    ///     println!("'{}'", my_char);
+    /// }
+    /// ```
+    pub fn random_special() -> char {
+        let ran_num: u8 = fastrand::u8(0..32);
+        SPECIAL_CHARACTERS[ran_num as usize]
+    }
 }
+
 
 #[cfg(test)]
 mod random_special {
@@ -88,45 +101,45 @@ mod random_special {
 
     #[test]
     fn first_special_char() {
-        let result = SPECIAL_CHARACTERS[0];
+        let result = strings::SPECIAL_CHARACTERS[0];
         assert_eq!(result, '~');
     }
 
     #[test]
     fn last_special_char() {
-        let result = SPECIAL_CHARACTERS[31];
+        let result = strings::SPECIAL_CHARACTERS[31];
         assert_eq!(result, '?');
     }
 
     #[test]
     fn backslash_char() {
-        let result = SPECIAL_CHARACTERS[20];
+        let result = strings::SPECIAL_CHARACTERS[20];
         assert_eq!(result, '\\');
     }
 
     #[test]
     fn double_quote_char() {
-        let result = SPECIAL_CHARACTERS[24];
+        let result = strings::SPECIAL_CHARACTERS[24];
         assert_eq!(result, '\"');
     }
 
     #[test]
     fn quote_char() {
-        let result = SPECIAL_CHARACTERS[25];
+        let result = strings::SPECIAL_CHARACTERS[25];
         assert_eq!(result, '\'');
     }
 
     #[test]
     fn first_char_with_fastrand() {
         let ran_num: u8 = fastrand::u8(0..1);
-        let result = SPECIAL_CHARACTERS[ran_num as usize];
+        let result = strings::SPECIAL_CHARACTERS[ran_num as usize];
         assert_eq!(result, '~');
     }
 
     #[test]
     fn last_char_with_fastrand() {
         let ran_num: u8 = fastrand::u8(31..32);
-        let result = SPECIAL_CHARACTERS[ran_num as usize];
+        let result = strings::SPECIAL_CHARACTERS[ran_num as usize];
         assert_eq!(result, '?');
     }
 }
